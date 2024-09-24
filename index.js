888d3e116234ef4d422af83f937518289047e00b
Baskoro Ramadhan <124755580+BaskoroR725@users.noreply.github.com>
2024-09-24 16:05:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -182,5 +182,5 @@ app.use((err,req,res,next) =>{
 const port = process.env.PORT || 3000;
 
 app.listen(port, () =>{
-    console.log('Listening on port 3000');
+    console.log(`Listening on port ${port}`);
 });
